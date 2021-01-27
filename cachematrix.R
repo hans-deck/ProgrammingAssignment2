8962d765a58db05566fca25c5b9cb3001e946131
hans-deck <hans.deck@gmail.com>
2021-01-27 11:58:55 +0100

    Program Assessment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makecacheMatrix <- function (m = matrix()) {
+        
+        m.inv <- NULL
+        set <- function(mat) {
+                if(!ïdentical(mat,m)) {
+                        
+                        m <<- mat
+                        m.inv <<- NULL    
+                        
+                }
+        }
+        setInvrse <- function(invrse) m.inv <<- invrse
+        get <- function() m
+        getInvrse <- function() m.inv
+        
+        list( set = set, setInv = setInvrse,
+              get = get, getInv = getInvrse)
+        
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        m.inv <- x$getInv()
+        
+        if(!is.null(m.inv)) {
+                message("getting cached inverse")
+        }
+        else {
+                m.sv <- x$get()
+                m.inv <- solve(m.sv, ...)
+                x$setInv(m.inv)
+        }
+        
+        m.inv
 }
